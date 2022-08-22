8f5d0d70f701c2bbe3f77fd2f88a4ec00eb50ba2
jinwood <jinw@protonmail.com>
2022-08-22 19:42:00 +0100

    Search API

--- routes/index.tsx
+++ routes/index.tsx
@@ -2,10 +2,12 @@
 import { h } from "preact";
 import { tw } from "@twind";
 import Counter from "../islands/Counter.tsx";
+import SearchBar from "../islands/SearchBar.tsx";
 
 export default function Home() {
   return (
     <div class={tw`p-4 mx-auto max-w-screen-md`}>
+      <SearchBar />
       <img
         src="/logo.svg"
         height="100px"
