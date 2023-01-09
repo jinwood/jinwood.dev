6931cabe9d211e1908c01fdf8461e064a2a863e5
jinwood <jinw@protonmail.com>
2023-01-09 22:19:31 +0000

    Lol

--- routes/[slug].tsx
+++ routes/[slug].tsx
@@ -17,24 +17,27 @@ export default function Post(props: PageProps) {
   const { pathname } = url;
   const post = props.data;
   return (
-    <Layout pathname={pathname} title={post.title}>
+    <>
       <Head>
         <style dangerouslySetInnerHTML={{ __html: CSS }} />
       </Head>
-      <main class="max-w-screen-md px-4 pt-16 mx-auto">
-        <h1 class="text-5xl font-bold">{post.title}</h1>
-        <time class="text-gray-500">
-          {new Date(post.publishedAt).toLocaleDateString("en-us", {
-            year: "numeric",
-            month: "long",
-            day: "numeric",
-          })}
-        </time>
-        <div
-          class="mt-8"
-          dangerouslySetInnerHTML={{ __html: render(post.content) }}
-        />
-      </main>
-    </Layout>
+
+      <Layout pathname={pathname} title={post.title}>
+        <main class="max-w-screen-md px-4 pt-16 mx-auto">
+          <h1 class="text-5xl font-bold">{post.title}</h1>
+          <time class="text-gray-500">
+            {new Date(post.publishedAt).toLocaleDateString("en-us", {
+              year: "numeric",
+              month: "long",
+              day: "numeric",
+            })}
+          </time>
+          <div
+            class="mt-8"
+            dangerouslySetInnerHTML={{ __html: render(post.content) }}
+          />
+        </main>
+      </Layout>
+    </>
   );
 }
--- routes/index.tsx
+++ routes/index.tsx
@@ -20,6 +20,7 @@ export default function BlogIndexPage(props: PageProps<Post[]>) {
   return (
     <Layout pathname={pathname} title="Home">
       <main class="max-w-screen-md px-4 pt-16 mx-auto">
+        <h1 class="text-5xl font-bold">I am super hot 🥵</h1>
         <div class="mt-8">
           {posts?.map((post) => (
             <PostCard post={post} />
