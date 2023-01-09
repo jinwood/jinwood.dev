390dc2ce2a75de7884038a72bb9dd6367a6977f5
jinwood <jinw@protonmail.com>
2023-01-09 22:22:01 +0000

    Revert lol

--- routes/index.tsx
+++ routes/index.tsx
@@ -20,8 +20,6 @@ export default function BlogIndexPage(props: PageProps<Post[]>) {
   return (
     <Layout pathname={pathname} title="Home">
       <main class="max-w-screen-md px-4 pt-16 mx-auto">
-        <h1 class="text-5xl font-bold">I am super hot 🥵</h1>
-        <h1 class="text-5xl font-bold">And I love Day ❤️</h1>
         <div class="mt-8">
           {posts?.map((post) => (
             <PostCard post={post} />
